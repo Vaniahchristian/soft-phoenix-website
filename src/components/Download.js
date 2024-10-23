a62b31b8fec1cf96bb87cfbb2219d55c33cb1a8d
Vaniah Christian Mukisa <mukisa.vaniah@students.mak.ac.ug>
2024-10-23 03:03:31 +0300

    apk

--- src/components/Download.js
+++ src/components/Download.js
@@ -5,7 +5,7 @@ const Download = () => {
     <section id="download" className="py-12 bg-gray-200">
       <h3 className="text-3xl font-bold text-center">Download Trade Mate</h3>
       <div className="mt-6 text-center">
-        <a href="/IAS (1).cmd" download>
+        <a href="/fluttersamples.apk" download>
           <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
             Download APK
           </button>
